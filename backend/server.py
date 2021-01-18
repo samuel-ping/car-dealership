3530089a17c0f919cbc9ac84c1212a633cd5ab42
Sam Ping <samuelping42@gmail.com>
2021-01-18 14:47:23 -0500

    add routes for getting listing by ID and getting stats for listings; added code for adding and deleting listings, but not tested yet

--- backend/server.py
+++ backend/server.py
@@ -9,8 +9,11 @@ db = client.car_listings
 
 app = Flask(__name__)
 
-@app.route('/listings')
-def listings():
+@app.route('/listings', methods=['GET'])
+def get_listings():
+    """
+    Retrieves all listings
+    """
     # https://www.geeksforgeeks.org/convert-pymongo-cursor-to-json/
     listings = db.listings.find()
     listings_json = list(listings)
@@ -18,10 +21,44 @@ def listings():
 
     return json_data
 
-@app.route('/listings/<_id>')
-def listings_url(_id):
-    listing_id = request.args.get('last', '')
-    return f'{_id}'
+@app.route('/listings/<_id>', methods=['GET'])
+def get_listing(_id):
+    """
+    Finds listing by ID
+    """
+    listing = db.listings.find({"_id": ObjectId(_id)})
+
+    listing_json = list(listing)
+    json_data = dumps(listing_json)
+
+    return json_data
+
+@app.route('/listings/stats', methods=['GET'])
+def get_stats():
+    """
+    Gets number of listings for each make
+    """
+    
+    make_stats = db.listings.aggregate([
+        { "$group": {"_id": "$make", "num_listings": { "$sum": 1 }}}
+    ])
+
+    make_stats_json = list(make_stats)
+    json_data = dumps(make_stats_json)
+
+    return json_data
+
+# @app.route("/listings/<_id>", methods=['DELETE'])
+# def mark_sold(_id):
+#     """
+#     Deletes a listing (marking it sold)
+#     """
+#     db.listings.deleteOne({"_id":_id})
+
+# @app.route("/listings", methods=['POST'])
+# def add_listing():
+#     listing_info = request.values()
+#     db.listings.insert_one(listing_info)
 
 @app.route('/hello')
 def hello():
